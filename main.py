cd7859c80da0bccb39209f2977f613401ca0912c
Abdallah-els <elsherbiny2023@gmail.com>
2025-09-15 12:32:45 +0300

    add movement functions to main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# #!/usr/bin/env python3
-# import rospy
-# import sensors
-# import pid_controller
-# from Computer_Vision_Functions.cv import initialize_cv, handle_detection, switch_to_letters, switch_to_signs
+#!/usr/bin/env python3
+import rospy
+import sensors
+import pid_controller
+from sensor_msgs.msg import String
+from Computer_Vision_Functions.cv import initialize_cv, handle_detection, switch_to_letters, switch_to_signs
 
 # def usage_examples():
 
@@ -40,24 +41,23 @@
 
 #     # main logic
 
-#!/usr/bin/env python3
-import rospy
-from geometry_msgs.msg import Twist
-from std_msgs.msg import String
-import sensors
-from Computer_Vision_Functions.cv import initialize_cv, handle_detection, switch_to_letters
+# #!/usr/bin/env python3
+# import rospy
+# from geometry_msgs.msg import Twist
+# import sensors
+# from Computer_Vision_Functions.cv import initialize_cv, handle_detection, switch_to_letters
 
 # --- Globals ---
 score = 0
 collected_letters = []
 cmd_pub = None
 
-def move_robot(linear=0.2, angular=0.0):
-    """Publish velocity commands to robot."""
-    twist = Twist()
-    twist.linear.x = linear
-    twist.angular.z = angular
-    cmd_pub.publish(twist)
+# def move_robot(linear=0.2, angular=0.0):
+#     """Publish velocity commands to robot."""
+#     twist = Twist()
+#     twist.linear.x = linear
+#     twist.angular.z = angular
+#     cmd_pub.publish(twist)
 
 def detection_callback(msg):
     """Handle detections from CV node."""
@@ -82,35 +82,28 @@ def detection_callback(msg):
 
     elif action == "turn" and value:
         if value == "right":
-            move_robot(0, -0.5)
+            pid_controller.turn_right()
             rospy.loginfo("Turned Right")
+
         elif value == "left":
-            move_robot(0, 0.5)
+            pid_controller.turn_left
             rospy.loginfo("Turned Left")
 
     else:
-        # No detection → navigate with sensors
-        front = sensors.f_ultrasonic()
-        if front is None:
-            return
-        if front > 25:  # clear path
-            yaw_error = 0 - sensors.yaw_degree()  # keep straight
-            angular_correction = 0.01 * yaw_error
-            move_robot(0.2, angular_correction)
-        else:
-            rospy.loginfo("Obstacle detected → turning left")
-            move_robot(0, 0.5)
+        pid_controller.forward()
+        rospy.loginfo("Turned Left")
+
 
 def main():
     global cmd_pub
     rospy.init_node("maze_main_node")
 
     # Publisher & Subscriber
-    cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     rospy.Subscriber("/detections", String, detection_callback)
 
-    # Start sensor node
+    # Start nodes
     sensors.start_node()
+    pid_controller.start_node()
 
     # Start CV node
     initialize_cv()
